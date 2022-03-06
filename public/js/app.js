cedcb7474ac0c56a43c59872102ec5f53115f8a4
goddamnit <goddamnit_76@hotmail.com>
2022-03-06 11:04:26 +0000

    Removed unnecessary clg

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-// console.log("client side js file loaded")
-
 const weatherForm = document.querySelector("form");
 const input = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
